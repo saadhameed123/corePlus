735eeffbebe2678d74c80cb3462c5a10d8fef843
saadhameed123 <125905167+saadhameed123@users.noreply.github.com>
2023-02-20 21:29:08 +0500

    nn

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+public class Class1
+{
+    public class employeeDetail()
+	{
+		
+	 using (var db = new veriparkDBContext())
+		{cascaac
---
+++ Enum.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class enums
+{
+    public enum BookStatus
+    {
+        Available = 0,
+        NotAvailable = 1,
+    }
+}
